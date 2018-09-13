5a5bc55632cce17c536692387ab97407e6458ba7
peterianmuller <peterianmuller@gmail.com>
2018-09-13 09:47:03 -0700

    Finish 1st verse lyric video for I do

--- units/music-player-with-lyrics/index.js
+++ units/music-player-with-lyrics/index.js
@@ -4,28 +4,6 @@ var _data = JSON.parse(
    `{"lyrics":[{"line":"","time":-1},{"line":"Hey, let's all go into the forest","note":"Verse 1","time":16000},{"line":"Nobody will notice for a while","time":20000},{"line":"There we can visit all the creatures","time":24000},{"line":"Maybe they can teach us facts of life","time":27500},{"line":"","time":32000},{"line":"Or we can travel to the ocean","note":"Verse 2","time":55500},{"line":"Don't forget your lotion","time":59500},{"line":"It's quite hot","time":61500},{"line":"I once met seven lovely crabs","time":64000},{"line":"They said I should go back and join them for tea","time":67500},{"line":"","time":72000},{"line":"Oh wait, the forest got demolished","note":"Verse 3","time":95500},{"line":"When they built the airport years ago","time":99000},{"line":"But we can still go see the ocean","time":103500},{"line":"Cause they put it in a bowl at the mall","time":107500},{"line":"","time":112000}]}`
 );
 
-var _data = JSON.parse(
-   `{"lyrics":[{"line":"","time":-1},
-               {"line":"Present simple","note":"Verse 1","time":4000},
-               {"line":"I go you go he goes","time":5900},
-               {"line":"she goes it goes you go","time":8800},
-               {"line":"they go and we go together","time":11800},
-               {"line":"","time":16300},
-               {"line":"present continuous","time":16500},
-               {"line":"I am going you are going he is going","time":18000},
-               {"line":"she is going it is going you are going","time":21000},
-               {"line":"they are going","time":24000},
-               {"line":"we are going together","time":25900},
-               {"line":"","time":27800},
-               {"line":"simple past","time":29000},
-               {"line":"I went you went","time":30500},
-               {"line":"he went she went it went","time":32000},
-               {"line":"you went","time":35800},
-               {"line":"Cause they put it in a bowl at the mall","time":107500},
-               {"line":"","time":112000}]}`
-);
-console.log(tncData[12].lyricsPlayer);
-
 var currentLine = '';
 
 function align() {
